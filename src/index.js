69c358bc5f44373cf1b75b2258a9669261d770ca
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2022-09-08 10:46:24 +0600

    updated router

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ import RootApp from './RootApp';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <RootApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
